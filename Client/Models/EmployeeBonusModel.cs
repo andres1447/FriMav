82022e6723af6a5669d6cb09fef377c83d42bbd0
Andrés Estevez <andres1447@gmail.com>
2022-11-21 21:22:54 -0300

    aguinaldo

---
+++ Client/Models/EmployeeBonusModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriMav.Client.Models
+{
+    public class EmployeeBonusModel : EmployeeDocumentModel
+    {
+        public decimal Salary { get; set; }
+
+        public decimal Amount() => Salary / 2;
+    }
+}
